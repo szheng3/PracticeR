f32dda033fc028b59e923877543e36226772e746
Sept.Griffith <szheng3@uci.edu>
2018-03-10 16:27:46 -0800

    added the 404 page

--- src/AppRoute/AppRoute.js
+++ src/AppRoute/AppRoute.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import HelpPage from '../component/HelpPage';
 import CreatePage from "../component/CreatePage";
 import EditorPage from "../component/EditorPage";
 import DashBoard from "../component/DashBoard";
+import NotFoundPage from "../component/NotFoundPage";
 
 
 const AppRoute = (props) => {
 
     return (
         <BrowserRouter>
-            <div>
+            <Switch>
                 <Route path={`/`} exact={true} component={DashBoard}/>
                 <Route path={`/create`} component={CreatePage}/>
                 <Route path={`/edit`} component={EditorPage}/>
                 <Route path={`/help`} component={HelpPage}/>
+                <Route component={NotFoundPage}/>
 
-            </div>
+            </Switch>
         </BrowserRouter>
     );
 
---
+++ src/component/NotFoundPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class NotFoundPage extends Component {
+    render() {
+        return (
+            <div>
+                404
+            </div>
+        );
+    }
+}
+
+NotFoundPage.propTypes = {};
+
+export default NotFoundPage;
