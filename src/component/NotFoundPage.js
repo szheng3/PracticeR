d71bdc82b565df09581872f6d2fbd78730d2ed43
Sept.Griffith <szheng3@uci.edu>
2018-03-10 16:31:21 -0800

    added the link

--- src/component/NotFoundPage.js
+++ src/component/NotFoundPage.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class NotFoundPage extends Component {
     render() {
         return (
             <div>
-                404
+                404 Home <Link to={`/`}>Go Home</Link>
             </div>
         );
     }
