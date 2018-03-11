6c1167ff5ae39f3dc3545283a1ca877485a1a2d8
Sept.Griffith <szheng3@uci.edu>
2018-03-10 16:17:11 -0800

    added the compoent

--- src/AppRoute/AppRoute.js
+++ src/AppRoute/AppRoute.js
@@ -1,12 +1,9 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import HelpPage from '../component/HelpPage';
 import CreatePage from "../component/CreatePage";
-
-
-
-
+import EditorPage from "../component/EditorPage";
+import DashBoard from "../component/DashBoard";
 
 
 const AppRoute = (props) => {
@@ -14,9 +11,10 @@ const AppRoute = (props) => {
     return (
         <BrowserRouter>
             <div>
-                <Route path={`/`} exact={true} component={HelpPage}/>
+                <Route path={`/`} exact={true} component={DashBoard}/>
                 <Route path={`/create`} component={CreatePage}/>
-                <Route path={edit} component={}/>
+                <Route path={`/edit`} component={EditorPage}/>
+                <Route path={`/help`} component={HelpPage}/>
 
             </div>
         </BrowserRouter>
---
+++ src/component/DashBoard.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class DashBoard extends Component {
+    render() {
+        return (
+            <div>
+                this is DashBoard
+            </div>
+        );
+    }
+}
+
+DashBoard.propTypes = {};
+
+export default DashBoard;
